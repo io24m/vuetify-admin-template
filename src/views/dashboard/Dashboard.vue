b30fd753b907fa8c0cca2d5655d85380e8e8002a
io24m <io24m@outlook.com>
2020-11-25 19:45:09 +0800

    i

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,3 +1,27 @@
 <template>
-    <div>Dashboard</div>
+  <div>
+    <v-snackbar v-model="snackbar">
+      {{ text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+    <v-btn small color="primary"  @click="snackbar = true">测试</v-btn>
+    <div>d</div>
+  </div>
 </template>
+<script>
+export default {
+  data: () => ({
+    snackbar: false,
+    text: "Hello, I'm a snackbar",
+  }),
+  methods: {
+    cl() {},
+  },
+};
+</script>
