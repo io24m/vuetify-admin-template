504feaa794259fd153568baf095ec162b886262c
io24m <io24m@outlook.com>
2020-11-26 09:49:40 +0800

    i

--- src/components/layout/Menu.vue
+++ src/components/layout/Menu.vue
@@ -1,7 +1,44 @@
 <template>
   <v-list nav dense>
+    <template v-for="item in routers">
+      <template v-if="!hasChild(item.children)">
+        <v-list-item-group color="primary" :key="item.path">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon v-text="item.meta && item.meta.icon"> </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.name"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </template>
+      <template v-else>
+        <v-list-group
+          :key="item.path"
+          v-model="item.active"
+          :prepend-icon="item.meta && item.meta.icon"
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.name"></v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item-group>
+            <v-list-item v-for="child in item.children" :key="child.path">
+              <v-list-item-icon>
+                <v-icon v-text="child.meta && child.meta.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="child.name"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list-group>
+      </template>
+    </template>
     <!-- 一级 -->
-    <v-list-item-group v-model="selectedItem" color="primary">
+    <!-- <v-list-item-group v-model="selectedItem" color="primary">
       <v-list-item v-for="(item, i) in items0" :key="i">
         <v-list-item-icon>
           <v-icon v-text="item.icon"></v-icon>
@@ -11,9 +48,9 @@
         </v-list-item-content>
       </v-list-item>
     </v-list-item-group>
-    <v-divider></v-divider>
+    <v-divider></v-divider> -->
     <!-- 二级 -->
-    <v-list-group
+    <!-- <v-list-group
       v-for="item in items"
       :key="item.title"
       v-model="item.active"
@@ -34,100 +71,37 @@
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
-    </v-list-group>
-    <!-- <v-divider></v-divider> -->
-    <!-- 三级 -->
-    <!-- <v-list-group :value="true" prepend-icon="mdi-account-circle">
-      <template v-slot:activator>
-        <v-list-item-title>Users</v-list-item-title>
-      </template>
-
-      <v-list-group :value="true" no-action sub-group>
-        <template v-slot:activator>
-          <v-list-item-content>
-            <v-list-item-title>Admin</v-list-item-title>
-          </v-list-item-content>
-        </template>
-        <v-list-item-group>
-          <v-list-item v-for="([title, icon], i) in admins" :key="i" link>
-            <v-list-item-title v-text="title"></v-list-item-title>
-
-            <v-list-item-icon>
-              <v-icon v-text="icon"></v-icon>
-            </v-list-item-icon>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list-group>
-
-      <v-list-group no-action sub-group>
-        <template v-slot:activator>
-          <v-list-item-content>
-            <v-list-item-title>Actions</v-list-item-title>
-          </v-list-item-content>
-        </template>
-
-        <v-list-item v-for="([title, icon], i) in cruds" :key="i" link>
-          <v-list-item-title v-text="title"></v-list-item-title>
-
-          <v-list-item-icon>
-            <v-icon v-text="icon"></v-icon>
-          </v-list-item-icon>
-        </v-list-item>
-      </v-list-group>
     </v-list-group> -->
   </v-list>
 </template>
 <script>
 import { mapGetters } from "vuex";
-
 export default {
   data: () => ({
     selectedItem: null,
-    items0: [
-      { text: "My Files", icon: "mdi-folder" },
-      { text: "Shared with me", icon: "mdi-account-multiple" },
-    ],
-    items: [
-      {
-        action: "mdi-ticket",
-        items: [{ title: "List Item" }],
-        title: "Attractions",
-      },
-      {
-        action: "mdi-silverware-fork-knife",
-        items: [
-          { title: "Breakfast & brunch" },
-          { title: "New American" },
-          { title: "Sushi" },
-        ],
-        title: "Dining",
-      },
-      {
-        action: "mdi-school",
-        items: [{ title: "List Item" }],
-        title: "Education",
-      },
-      {
-        action: "mdi-run",
-        items: [{ title: "List Item" }],
-        title: "Family",
-      },
-    ],
-    admins: [
-      ["Management", "mdi-account-multiple-outline"],
-      ["Settings", "mdi-cog-outline"],
-    ],
-    cruds: [
-      ["Create", "mdi-plus-outline"],
-      ["Read", "mdi-file-outline"],
-      ["Update", "mdi-update"],
-      ["Delete", "mdi-delete"],
-    ],
   }),
   computed: {
     ...mapGetters({
-      routes: "permission/getRouters",
+      routers: "permission/getRouters",
     }),
+  },
+  methods: {
+    hasChild(children) {
+      if (!children) {
+        return false;
+      }
+      const childrens = children.filter((item) => {
+        if (item.hidden) {
+          return false;
+        } else {
+          // Temp set(will be used if only has one showing child)
+          //this.onlyOneChild = item;
+          return true;
+        }
+      });
+      return childrens.length !== 1;
+    },
+    openMenu() {},
   },
 };
 </script>
--- src/router/constantRouter.js
+++ src/router/constantRouter.js
@@ -7,7 +7,7 @@ const constantRouter = [{
     redirect: "/dashboard",
     component: Layout,
     meta: {
-        icon: "home"
+        icon: "mdi-folder"
     },
     children: [{
         path: "/dashboard",
@@ -15,17 +15,58 @@ const constantRouter = [{
     }]
 }, {
     name: "控制台",
-    path: "/",
+    path: "/1",
     redirect: "/table",
     component: Layout,
     meta: {
-        icon: "home"
+        icon: "mdi-silverware-fork-knife"
     },
     children: [{
+        name: '表格',
         path: "/table",
-        component: () => import("@/views/table/Table")
+        component: () => import("@/views/table/Table"),
+        meta: {
+            icon: "mdi-silverware-fork-knife"
+        },
     }, {
+        name: '表格1',
         path: "/table1",
+        component: () => import("@/views/table/Table"),
+        meta: {
+            icon: "mdi-run"
+        },
+    }]
+}, {
+    name: '邮件',
+    path: '/email',
+    component: Layout,
+    meta: {
+        icon: 'mdi-account-multiple'
+    },
+    children: [{
+        path: "/email",
+        component: () => import("@/views/table/Table")
+    }]
+}, {
+    name: '车票',
+    path: '/email1',
+    component: Layout,
+    meta: {
+        icon: 'mdi-ticket'
+    },
+    children: [{
+        path: "/email",
+        component: () => import("@/views/table/Table")
+    }]
+}, {
+    name: '学校',
+    path: '/email12',
+    component: Layout,
+    meta: {
+        icon: 'mdi-school'
+    },
+    children: [{
+        path: "/email",
         component: () => import("@/views/table/Table")
     }]
 }];
