e28cf874e9e644e1e4db7318977f860992d571ab
io24m <io24m@outlook.com>
2021-02-07 14:55:45 +0800

    i

--- src/views/validate/ValidateConfig.vue
+++ src/views/validate/ValidateConfig.vue
@@ -1,3 +1,76 @@
 <template>
-    <div>validateConfig</div>
+  <div>
+    <div v-for="(item, index) in configTree" :key="index">
+      {{ item.className }}
+    </div>
+    <div>-----------</div>
+
+    <div v-for="(item, index) in config" :key="index">
+      {{ item.className + "." + item.key + "." + item.validateType }}
+      <div>{{ item.check ? "验证" : "不验证" }}</div>
+      <div>{{ item.pass ? "跳过" : "不跳过" }}</div>
+      <div>{{ item.errormessage }}</div>
+    </div>
+
+    <v-treeview :items="configTree"></v-treeview>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      config: [],
+    };
+  },
+  watch: {},
+  computed: {
+    configTree() {
+      return this.g(this.config);
+    },
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.$http({
+        url: "/validate/config",
+      }).then((data) => {
+        this.config = data.result;
+        let t = this.g(this.config);
+        console.log(t);
+      });
+    },
+    getConfigClass() {},
+    g(c) {
+      if (!c) {
+        return;
+      }
+      let r = [];
+      c.forEach((item) => {
+        let children;
+        for (let i = 0; i < r.length; i++) {
+          if (item.key === r[i].key) {
+            children=r[i].children
+          }
+        }
+        if (children) {
+          children.push({
+            validateType: item.validateType,
+            check: item.check,
+            pass: item.pass,
+            errormessage: item.errormessage,
+          });
+          return;
+        }
+        r.push({
+          className: item.className,
+          key: item.key,
+          children: [],
+        });
+      });
+      return r;
+    },
+  },
+};
+</script>
--- src/views/validate/ValidateData.vue
+++ src/views/validate/ValidateData.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>validateData</div>
+  <div>
+    <v-btn depressed color="primary" @click="onPost" class="mb-4"> post </v-btn>
+    <v-textarea outlined label="params" v-model="person"></v-textarea>
+    <v-textarea outlined label="response" v-model="response"></v-textarea>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      person: "",
+      response: "",
+    };
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.person = JSON.stringify({
+        age: " ",
+        name: " ",
+      });
+    },
+    onPost() {
+      this.response = "";
+      this.response = this.person;
+    },
+  },
+};
+</script>
