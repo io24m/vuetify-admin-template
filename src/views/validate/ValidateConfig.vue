6b7517115f4a9a9039bf3ec84d61f6f686e71db2
io24m <io24m@outlook.com>
2021-02-08 16:47:56 +0800

    validate

--- src/views/validate/ValidateConfig.vue
+++ src/views/validate/ValidateConfig.vue
@@ -1,20 +1,18 @@
 <template>
   <div>
     <v-card>
-      <v-tabs
-        background-color="primary"
-        center-active
-        dark
-        @change="tabChange"
-      >
+      <v-tabs background-color="primary" center-active dark @change="tabChange">
         <v-tab v-for="(item, i) in classConfig" :key="i">{{
           item.className
         }}</v-tab>
       </v-tabs>
     </v-card>
-
+    <!-- 列表 -->
     <v-card class="flex-row mt-2" tile>
-      <v-card-subtitle>{{ classType }}</v-card-subtitle>
+      <!-- <v-card-subtitle>
+        <v-btn text color="primary"> 新增校验逻辑 </v-btn>
+        {{ classType }}</v-card-subtitle
+      > -->
       <v-data-table
         :headers="headers"
         :items="fileConfigs"
@@ -22,6 +20,14 @@
         class="elevation-1"
         hide-default-footer
       >
+        <template v-slot:top>
+          <v-toolbar flat>
+            {{ classType }}
+            <v-spacer></v-spacer>
+            <v-btn text color="primary"> 新增校验逻辑 </v-btn>
+          </v-toolbar></template
+        >
+
         <template v-slot:[`item.check`]="{ item }">
           <v-chip small :color="item.check ? 'green' : 'red'">
             {{ item.check ? "启用" : "禁用" }}
@@ -32,8 +38,78 @@
             {{ item.pass ? "不报错" : "报错" }}
           </v-chip>
         </template>
+
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
       </v-data-table>
     </v-card>
+    <!-- 修改 -->
+    <v-dialog v-model="editDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">title</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedItem.name"
+                  label="Dessert name"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedItem.calories"
+                  label="Calories"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedItem.fat"
+                  label="Fat (g)"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedItem.carbs"
+                  label="Carbs (g)"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedItem.protein"
+                  label="Protein (g)"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="editClose"> 取消 </v-btn>
+          <v-btn color="blue darken-1" text @click="editSave"> 保存 </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- 删除 -->
+    <v-dialog v-model="deleteDialog" max-width="500px">
+      <v-card>
+        <v-card-title class="headline">确认删除记录？</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="deleteClose">取消</v-btn>
+          <v-btn color="error" text @click="deleteConfirm">确认删除</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
@@ -63,8 +139,13 @@ export default {
           text: "错误信息",
           value: "errorMessage",
         },
+        { text: "操作", value: "actions", sortable: false },
       ],
       fileConfigs: [],
+      editDialog: false,
+      editedIndex: 0,
+      editedItem: { name: "", calories: 0, fat: 0, carbs: 0, protein: 0 },
+      deleteDialog: false,
     };
   },
   watch: {},
@@ -95,6 +176,24 @@ export default {
         this.fileConfigs = data.result;
       });
     },
+    editItem(item) {
+      this.editedIndex = this.fileConfigs.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.editDialog = true;
+    },
+    editClose() {
+      this.editDialog = false;
+    },
+    editSave() {},
+    deleteItem(item) {
+      this.editedIndex = this.fileConfigs.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.deleteDialog = true;
+    },
+    deleteClose() {
+      this.deleteDialog = false;
+    },
+    deleteConfirm() {},
   },
 };
 </script>
